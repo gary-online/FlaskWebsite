9eaaddfb85e10876f215f55fcdb402b584e1124f
gary <glessnergaryw@gmail.com>
2024-01-05 21:51:03 -0500

    updates

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ from datetime import datetime
 from logging import DEBUG
 from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = b'Ej~"\\5\xbf\x1c'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 app.logger.setLevel(DEBUG)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=true, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 feedback = []
 
